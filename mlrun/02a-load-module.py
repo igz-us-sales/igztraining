56cd2270789a810fe888a948eb500673cb5a6dcb
Marcelo <marcelol@iguazio.com>
2020-08-04 17:30:30 +0000

    MlRun day I

---
+++ mlrun/02a-load-module.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('./modules')
+
+from script_module_02a import save_property
+
+save_property()
--- mlrun/script_to_run.py
+++ mlrun/script_to_run.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import sys
+
+df = pd.read_csv(sys.argv[0])
 def handler(context):
     # Parameters
     param1 = context.get_param('param1')
