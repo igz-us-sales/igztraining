c76654455eda7378e7ed81afc77ca5913dbdbe59
Marcelo <marcelol@iguazio.com>
2020-09-01 18:57:02 +0000

    MLrun day 3 and 4

---
+++ mlrun/Day-3/modules/module_handler_functions.py
@@ -0,0 +1,26 @@
+def save_property(context, source_file='',target_file=''):
+
+    fl = open(source_file,'r')
+    target_fl = open(target_file,'w')
+
+    for line in fl:
+        if 'Condo' in line:
+            target_fl.write(line)
+
+
+    target_fl.close()
+
+    print(f"file saved to {target_file}")
+    
+    
+def filter_properties(context):
+    source_file = "/User/igztraining/mlrun/data/demo.csv"
+    target_file = "/User/igztraining/mlrun/data/condos.csv"
+
+    fl = open(source_file,'r')
+    target_fl = open(target_file,'w')
+
+    for line in fl:
+        if 'Condo' in line:
+            print(line)
+
---
+++ mlrun/Day-4/conf/workflow.py
@@ -0,0 +1,29 @@
+
+from kfp import dsl
+from mlrun import mount_v3io
+
+@dsl.pipeline(
+    name = 'Property Management',
+    description = 'Condo reviews'
+)
+
+# Configure function resources and local settings
+def init_functions(functions: dict, project=None, secrets=None):
+    for f in functions.values():
+        f.apply(mount_v3io())
+
+
+def kfpipeline(
+    source_file :str = ""
+) -> None:
+    
+    save_properties = funcs['properties-handler'].as_step(handler='save_property',
+                            params={"source_file" : source_file,
+                                    "target_file" : "/User/igztraining/mlrun/data/condos.csv" },
+                            outputs=['condo_properties'])
+    
+    
+    filter_properties = funcs['properties-handler'].as_step(handler='filter_properties',
+                                   params={"city" : "SACRAMENTO",
+                                           "target_dataset" : "sacramento_condos" },
+                                   inputs={"source_data":save_properties.outputs["condo_properties"]})
